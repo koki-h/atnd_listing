48d10a747bcbfedf28e4f4119c41e1624c4e5f21
koki <koki@koki-mbp.local>
2015-03-06 20:18:50 +0900

    Initial Commit.ATNDからTwitterIDを取得するところだけ作った

---
+++ lib/atnd.rb
@@ -0,0 +1,12 @@
+require 'mechanize'
+module ATND
+  EVENT_USERS_URI = "http://api.atnd.org/events/users/"
+  def self.event_members(event_id)
+    agent = Mechanize.new
+    res = agent.get(EVENT_USERS_URI + "?event_id=#{event_id}")
+    res.search("user/twitter_id").each do |tw_id|
+      yield(tw_id.text)
+    end
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,2 @@
+require './lib/atnd.rb'
+ATND.event_members("61150"){|id| puts id}
